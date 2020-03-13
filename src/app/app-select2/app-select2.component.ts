5f2361cd2b912052e36099e5f6462492f913c5dc
Xantras44 <santi.r.s4@gmail.com>
2020-03-13 13:17:30 +0100

    Funciona bien

--- src/app/app-select2/app-select2.component.ts
+++ src/app/app-select2/app-select2.component.ts
@@ -6,19 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app-select2.component.scss']
 })
 export class AppSelect2Component implements OnInit {
-  public Comunidades:Array<[string,number]>=[['Castilla',0],['Madrid',1]];
+  /*public Comunidades:Array<[string,number]>=[['Castilla',0],['Madrid',1]];
   public Comunidad:string="";
-  public Provincia:string="";
-  public a:string="";
+  public Provincia:string="";*/
+  comunidades = ['Galicia', 'Cataluna'];
+  provincias = []
+  provinciasElegidas = {'Galicia': ['Lugo', 'Ourense'], 'Cataluna': ['Girona', 'Tarragona'],}
+  //public a:string="";
   
+  cambioComunidad(dato){
+    this.provincias = this.provinciasElegidas[dato]
+  }
 
-  public getComunidades()
+  /*public getComunidades()
   {
     return this.Comunidades;
   }
   devolverProvincia() {
     
-  }
+  }*/
   constructor() { }
 
   ngOnInit() {
